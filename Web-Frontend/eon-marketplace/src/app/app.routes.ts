150cc346db616fa7da098a0824a2788e35209774
FelixKohlboeck <f.kohlboeck@students.htl-leonding.ac.at>
2025-07-22 17:43:58 +0200

    first steps, needed to commit to update angular for taiga ui

---
+++ Web-Frontend/eon-marketplace/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', pathMatch: 'full', component: HomePageComponent}
+];
