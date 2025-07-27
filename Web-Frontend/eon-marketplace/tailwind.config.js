83a24ea1b79c46eb95451d8cedb97621b087e626
FelixKohlboeck <f.kohlboeck@students.htl-leonding.ac.at>
2025-07-27 20:47:06 +0200

    new layout done and trying with style and background

--- Web-Frontend/eon-marketplace/tailwind.config.js
+++ Web-Frontend/eon-marketplace/tailwind.config.js
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'], // <-- deine Font
+        sans: ['Inter', 'sans-serif'],
       },
       colors: {
-        space: "#1F2041",
-        violet: "#4B3F72",
-        sun: "#FFC857",
-        cyan: "#119DA4",
-        graypayne: "#19647E",
+        deepblack: "#050505",
+        accent: "#FFB238",
+        goldlight: "#FFC95C",
+        white: "#FFFFFF",
+        softgray: "#ECECEC",
+      },
+      boxShadow: {
+        glow: "0 0 20px rgba(255, 178, 56, 0.4)",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'), // optional fancy content styles
-  ],
-}
-
-
+  plugins: [require('@tailwindcss/typography')],
+};
